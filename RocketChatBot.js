5b2ad4bd28092e77baf0f7874cfdabc96bc15eaf
ArthurTemporim <arthurrtl@gmail.com>
2018-05-31 11:53:49 -0300

    Make the connection with botkit.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- RocketChatBot.js
+++ RocketChatBot.js
@@ -1,61 +1,45 @@
-// Botkit imports, need to make it relative path
+// TODO: need to make it relative path
 var Botkit = require('./node_modules/botkit/lib/CoreBot.js');
-// RocketChat SDK
 const { driver } = require('@rocket.chat/sdk');
 
 const ROOMS = ['GENERAL'];
 const SSL = false;
 
 
-function RocketChatBot(botkit, config) {    
-    console.log("\nI'm in lib/RocketChatBot.js!!!\n")
-
+function RocketChatBot(botkit, config) {
+    console.log("Inside RocketChatBot");
+    
     var controller = Botkit(config || {});
 
     var myuserid;
-    // this simple bot does not handle errors, different messsage types, server resets 
-    // and other production situations 
 
-    controller.startBot = async () => {
-        console.log(config);
-        const conn = await driver.connect({ host: config.rocketchat_host, useSsl: SSL })
-        myuserid = await driver.login({ username: config.rocketchat_bot_user, password: config.rocketchat_bot_pass });
-        const roomsJoined = await driver.joinRooms(ROOMS);
-        console.log('joined rooms');
-
-        // set up subscriptions - rooms we are interested in listening to
-        const subscribed = await driver.subscribeToMessages();
-        console.log('subscribed');
-
-        // connect the processMessages callback
-        const msgloop = await driver.reactToMessages(processMessages);
-        console.log('connected and waiting for messages');
-
-        // when a message is created in one of the ROOM, we 
-        // receive it in the processMesssages callback
-
-        // greets from the first room in rooms
-        const sent = await driver.sendToRoom(config.rocketchat_bot_user + ' is listening ...', ROOMS[0]);
-        console.log('Greeting message sent');
+    controller.startBot = async () => {        
+        // insert to var bot bot.defineBot()
+        var bot = controller.spawn(config);
+        try {
+            // make the connection with RocketChat
+            const conn = await driver.connect({ host: config.rocketchat_host, useSsl: SSL })
+            myuserid = await driver.login({ username: config.rocketchat_bot_user, password: config.rocketchat_bot_pass });
+            const roomsJoined = await driver.joinRooms(ROOMS);
+            console.log('joined rooms');
+            // set up subscriptions - rooms we are interested in listening to
+            const subscribed = await driver.subscribeToMessages();
+            console.log('subscribed');
+            // TODO: need to verify the real connection
+            bot.connected = true;
+        } catch (error) {
+            bot.connected = false;
+            console.log(error);
+        }
+        // send the first message to channel
+        bot.send(config.rocketchat_bot_user + " is listening...");
+        // test a reply message in channel
+        bot.reply("reply message", 'ok');
     }
 
-    // callback for incoming messages filter and processing
-    const processMessages = async (err, message, messageOptions) => {
-        if (!err) {
-            // filter our own message
-            if (message.u._id === myuserid) return;
-            // can filter further based on message.rid
-            const roomname = await driver.getRoomName(message.rid);
-            if (message.msg.toLowerCase().startsWith(config.rocketchat_bot_user)) {
-                const response = message.u.username +
-                    ', how can ' + config.rocketchat_bot_user + ' help you with ' +
-                    message.msg.substr(config.rocketchat_bot_user.length + 1);
-                const sentmsg = await driver.sendToRoom(response, roomname);
-            }
-        }
-    }   
-
+    // TODO: config it's not getting the correct values inside defineBot
     controller.defineBot(function (botkit, config) {
+        console.log('Inside defineBot');
         var bot = {
             type: 'rocketchat',
             botkit: botkit,
@@ -63,15 +47,29 @@ function RocketChatBot(botkit, config) {
             utterances: botkit.utterances,
         }
 
-        // here is where you make the API call to SEND a message
-        // the message object should be in the proper format already
-        bot.send = function (message, cb) {
-            console.log('\n\n\nSEND: ', message);
+        bot.startConversation = function(message, cb) {
+            botkit.startConversation(this, message, cb);
+        };
+
+        bot.createConversation = function(message, cb) {
+            botkit.createConversation(this, message, cb);
+        };
+        
+        bot.send = async function (message, cb) {                        
+            if(bot.connected) {
+                // TODO: need to configure the channel parameter
+                const sent = await driver.sendToRoom(message, ROOMS[0]);            
+                console.log('SEND: ', message);
+            }            
         }
 
         // this function takes an incoming message (from a user) and an outgoing message (reply from bot)
         // and ensures that the reply has the appropriate fields to appear as a reply
         bot.reply = function (src, resp, cb) {
+            console.log('reply')
+            console.log("\n\n src: "+src);
+            console.log("\n\n resp: "+resp);
+            console.log("\n\n cb: "+cb);
             var msg = {};
 
             if (typeof (resp) == 'string') {
@@ -83,6 +81,7 @@ function RocketChatBot(botkit, config) {
             msg.channel = src.channel;
             msg.to = src.user;
 
+            console.log("\n bot.say: "+bot.say(msg, cb))
             bot.say(msg, cb);
         };
 
@@ -136,7 +135,7 @@ function RocketChatBot(botkit, config) {
     };
 
 
-    return controller;    
+    return controller;
 }
 
 module.exports = RocketChatBot;
