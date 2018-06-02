1d86f4761a196a68ed0462df77b42e0bf815b70e
ArthurTemporim <arthurrtl@gmail.com>
2018-06-02 12:30:53 -0300

    Update reply message to get RocketChat messages.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- RocketChatBot.js
+++ RocketChatBot.js
@@ -34,7 +34,7 @@ function RocketChatBot(botkit, config) {
         // send the first message to channel
         bot.send(config.rocketchat_bot_user + " is listening...");
         // test a reply message in channel
-        bot.reply({}, 'ok');
+        bot.reply({}, 'biscoito');
     }
 
     // TODO: config it's not getting the correct values inside defineBot
@@ -57,34 +57,55 @@ function RocketChatBot(botkit, config) {
 
         bot.send = async function (message, cb) {
             console.log("\ninside bot.send")
+            console.log("\nmessage: "+message.text)
             if (bot.connected) {
                 // TODO: need to configure the channel parameter                
                 const sent = await driver.sendToRoom(message, ROOMS[0]);
-                console.log('SEND: ', message);                
+                console.log('SEND: ', message);
             }
         }
 
         // this function takes an incoming message (from a user) and an outgoing message (reply from bot)
         // and ensures that the reply has the appropriate fields to appear as a reply
-        bot.reply = function (src, resp, cb) {
+        bot.reply = async function (src, resp, cb) {          
+            // callback for incoming messages filter and processing
+            const processMessages = async (err, message, messageOptions) => {
+                if (!err) {
+                    // filter our own message
+                    if (message.u._id === myuserid) return;
+                    // can filter further based on message.rid
+                    const roomname = await driver.getRoomName(message.rid);
+                    const response = 'I receive this message: ' + resp.text
+                    const sentmsg = await driver.sendToRoom(response, roomname);
+                }
+            }           
+            
+            // connect the processMessages callback
+            const msgloop = await driver.reactToMessages(processMessages);
+            console.log('connected and waiting for messages');
+
             console.log('\ninside reply')
+            console.log("\nsrc.channel: " + src.text);
             if (typeof (resp) == 'string') {
                 resp = {
                     text: resp
                 };
             }
 
-            resp.channel = src.channel;
+            resp.channel = 'socket';
             resp.to = src.user;
 
-            console.log("\n\nbefore bot.say")
+            console.log("\nbefore bot.say")
 
             bot.say(resp, cb);
+
+            console.log("\nafter bot.say")
         };
 
         // this function defines the mechanism by which botkit looks for ongoing conversations
         // probably leave as is!
         bot.findConversation = function (message, cb) {
+            console.log("\ninside bot.findConversation")
             for (var t = 0; t < botkit.tasks.length; t++) {
                 for (var c = 0; c < botkit.tasks[t].convos.length; c++) {
                     if (
@@ -102,13 +123,42 @@ function RocketChatBot(botkit, config) {
         return bot;
     })
 
-    controller.middleware.format.use(function(bot, message, platform_message, next) {
+    // provide one or more normalize middleware functions that take a raw incoming message
+    // and ensure that the key botkit fields are present -- user, channel, text, and type
+    controller.middleware.normalize.use(function (bot, message, next) {
+
+        console.log('NORMALIZE', message);
+        next();
+
+    });
+
+    controller.middleware.format.use(function (bot, message, platform_message, next) {
         for (var k in message) {
             platform_message[k] = message[k]
-          }
+        }
         next();
-        console.log("\n\ninside middleware.format.use\n")
+        console.log("\ninside middleware.format.use")
     });
+
+    controller.middleware.categorize.use(function(bot, message, next) {
+        console.log("\ninside middleware.categorize.use")
+        if (message.type == 'message') {
+            message.type = 'message_received';
+        }
+
+        next();
+
+    });
+
+    // provide a way to receive messages - normally by handling an incoming webhook as below!
+    controller.handleWebhookPayload = function (req, res) {        
+        // var payload = req.body;
+
+        // var bot = controller.spawn({});
+        // controller.ingest(bot, payload, res);
+
+        // res.status(200);
+    };
 
     return controller;
 }
