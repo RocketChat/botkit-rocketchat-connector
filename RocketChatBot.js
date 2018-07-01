5cc391ff31d7f0cbcc8c4c06c702188601447563
ArthurTemporim <arthurrtl@gmail.com>
2018-07-01 15:29:05 -0300

    Remove wrong use of async/await and unnecessary logs.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- RocketChatBot.js
+++ RocketChatBot.js
@@ -2,14 +2,12 @@ var Botkit = require('botkit');
 const { driver } = require('@rocket.chat/sdk');
 
 function RocketChatBot(botkit, config) {
-    console.log("Inside RocketChatBot");
-    // get the "brain" of Botkit
     var controller = Botkit.core(config || {});
     // transform the string value from .env to bool.
     var SSL = (config.rocketchat_ssl === 'true')
 
     controller.startBot = async () => {
-        // insert to var bot bot.defineBot()
+        // implicit call for bot.defineBot()
         var bot = controller.spawn(config);
         try {
             // make the connection with RocketChat
@@ -24,7 +22,6 @@ function RocketChatBot(botkit, config) {
         }
 
         if (bot.connected) {
-            
             var options = {
                 dm: config.rocketchat_bot_direct_messages,
                 livechat: config.rocketchat_bot_live_chat,
@@ -33,14 +30,11 @@ function RocketChatBot(botkit, config) {
 
             // trigger when every message is sent from any source enabled from
             // options
-            driver.respondToMessages(async function (err, message, meta) {
-                console.log("\ninside respondToMessages");
-                console.log(meta)
-
+            driver.respondToMessages(function (err, message, meta) {
                 // store the text from RocketChat incomming messages
                 // this message is already normalized.
                 // but we might be missing out on fields we want 
-                var messageSource = await getMessageSource(meta);
+                var messageSource = getMessageSource(meta);
                 var incommingMessage = {
                     text: message.msg,
                     user: message.u.username,
@@ -48,18 +42,12 @@ function RocketChatBot(botkit, config) {
                     type: messageSource,
                     ts: message.ts.$date
                 }
-
-                try {
-                    await controller.ingest(bot, incommingMessage)
-                } catch (err) {
-                    console.log(err)
-                }
+                controller.ingest(bot, incommingMessage)
             }, options);
         }
     }
 
     controller.defineBot(function (botkit, config) {
-        console.log('Inside defineBot');
         var bot = {
             type: 'rocketchat',
             botkit: botkit,
@@ -67,29 +55,51 @@ function RocketChatBot(botkit, config) {
             utterances: botkit.utterances,
         }
 
-        bot.send = async function(message, cb) {
-            console.log("\ninside bot.send")
+        bot.send = function (message, cb) {
             console.log(message)
             if (bot.connected) {
+                // handles every type of message
                 if (message.type === 'directMessage') {
-                      await driver.sendDirectToUser(message.text, message.user);
+                    driver.sendDirectToUser(message.text, message.user)
+                        .then(() => {
+                            cb()
+                        })
+                        .catch((error) => {
+                            console.log(error)
+                        })
                 } else if (message.type === 'liveChat') {
                     // TODO: implement answer to livechat
                 } else if (message.type === 'privateChannel') {
-                      await driver.sendToRoomId(message.text, message.channel);
+                    driver.sendToRoomId(message.text, message.channel)
+                        .then(() => {
+                            cb()
+                        })
+                        .catch((error) => {
+                            console.log(error)
+                        })
                 } else if (message.type === 'channel') {
-                      await driver.sendToRoomId(message.text, message.channel);
+                    driver.sendToRoomId(message.text, message.channel)
+                        .then(() => {
+                            cb()
+                        })
+                        .catch((error) => {
+                            console.log(error)
+                        })
                 } else if (message.type === 'message') {
-                      await driver.sendToRoomId(message.text, message.channel);
-                }  
-                cb();
+                    driver.sendToRoomId(message.text, message.channel)
+                        .then(() => {
+                            cb()
+                        })
+                        .catch((error) => {
+                            console.log(error)
+                        })
+                }
             }
-            //  BOT is not connected
+            // BOT is not connected
             cb();
         }
 
-        bot.reply = async function (src, resp, cb) {
-            console.log('\ninside reply')
+        bot.reply = function (src, resp, cb) {
             if (typeof (resp) == 'string') {
                 resp = {
                     text: resp
@@ -104,52 +114,46 @@ function RocketChatBot(botkit, config) {
         // this function defines the mechanism by which botkit looks for ongoing conversations
         // probably leave as is!
         bot.findConversation = function (message, cb) {
-            console.log("\nInside findConversation")
-                for (var t = 0; t < botkit.tasks.length; t++) {
-                    console.log(botkit.tasks.length)
-                    for (var c = 0; c < botkit.tasks[t].convos.length; c++) {
-                        console.log(botkit.tasks[t].convos[c].source_message)
-                        // the ts just is added to source_message.original_message.ts
-                        if (
-                            botkit.tasks[t].convos[c].isActive() &&
-                            botkit.tasks[t].convos[c].source_message.user == message.user &&
-                            botkit.tasks[t].convos[c].source_message.channel == message.channel &&
-                            botkit.excludedEvents.indexOf(message.type) == -1 // this type of message should not be included
-                        ) {
-                            cb(botkit.tasks[t].convos[c]);
-                            return;
-                        }
+            for (var t = 0; t < botkit.tasks.length; t++) {
+                for (var c = 0; c < botkit.tasks[t].convos.length; c++) {
+                    if (
+                        botkit.tasks[t].convos[c].isActive() &&
+                        botkit.tasks[t].convos[c].source_message.user == message.user &&
+                        botkit.tasks[t].convos[c].source_message.channel == message.channel &&
+                        botkit.excludedEvents.indexOf(message.type) == -1 // this type of message should not be included
+                    ) {
+                        cb(botkit.tasks[t].convos[c]);
+                        return;
                     }
+                }
             }
             cb();
         };
         return bot;
     })
 
-    controller.middleware.receive.use(function(bot, message, next) { console.log('I RECEIVED', message); next(); });
-
-    controller.middleware.send.use(function(bot, message, next) { console.log('I AM SENDING', message); next(); });
+    // Verify the pipeline of the message.
+    //controller.middleware.receive.use(function (bot, message, next) { console.log('I RECEIVED', message); next(); });
+    //controller.middleware.send.use(function (bot, message, next) { console.log('I AM SENDING', message); next(); });
 
     // provide one or more normalize middleware functions that take a raw incoming message
     // and ensure that the key botkit fields are present -- user, channel, text, and type
     controller.middleware.normalize.use(function (bot, message, next) {
         next();
     });
-     controller.middleware.categorize.use(function (bot, message, next) {
-        console.log("\n*inside middleware.categorize.use");
+
+    controller.middleware.categorize.use(function (bot, message, next) {
         if (message.type == 'message') {
             message.type = 'message_received';
         }
         next();
     });
 
-  
+
     // provide one or more ways to format outgoing messages from botkit messages into 
     // the necessary format required by the platform API
     // at a minimum, copy all fields from `message` to `platform_message`
     controller.middleware.format.use(function (bot, message, platform_message, next) {
-        console.log("\n*inside middleware.format.use")
-        console.log(message)
         for (var k in message) {
             platform_message[k] = message[k]
         }
@@ -160,7 +164,7 @@ function RocketChatBot(botkit, config) {
     });
 
     // Utils functions
-    async function getMessageSource(meta) {
+    function getMessageSource(meta) {
         var messageSource;
         if (meta.roomType === 'd') {
             messageSource = 'directMessage';
