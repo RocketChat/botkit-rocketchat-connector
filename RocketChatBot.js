91fcb010e76a8999ec69b3185b3b5df969446053
ArthurTemporim <arthurrtl@gmail.com>
2018-07-10 05:29:49 -0300

    Add eslint.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- RocketChatBot.js
+++ RocketChatBot.js
@@ -1,145 +1,144 @@
-var Botkit = require('botkit');
-const { driver } = require('@rocket.chat/sdk');
-const utils = require('./utils');
+var Botkit = require('botkit')
+const { driver } = require('@rocket.chat/sdk')
+const utils = require('./utils')
 
-function RocketChatBot(botkit, config) {
-    var controller = Botkit.core(config || {});
-    // transform the string value from .env to bool.
-    var SSL = (config.rocketchat_ssl === 'true')
+function RocketChatBot (botkit, config) {
+  var controller = Botkit.core(config || {})
+  // transform the string value from .env to bool.
+  var SSL = (config.rocketchat_ssl === 'true')
 
-    controller.startBot = async () => {
-        // implicit call for bot.defineBot()
-        var bot = controller.spawn(config);
-        try {
-            // make the connection with RocketChat
-            await driver.connect({ host: config.rocketchat_host, useSsl: SSL })
-            await driver.login({ username: config.rocketchat_bot_user, password: config.rocketchat_bot_pass });
-            await driver.joinRooms([config.rocketchat_bot_room]);
-            await driver.subscribeToMessages();
-            bot.connected = true;
-        } catch (error) {
-            bot.connected = false;
-            console.log(error);
-        }
-
-        if (bot.connected) {
-            var options = {
-                dm: config.rocketchat_bot_direct_messages,
-                livechat: config.rocketchat_bot_live_chat,
-                edited: config.rocketchat_bot_edited
-            }
-
-            // trigger when every message is sent from any source enabled from
-            // options
-            driver.respondToMessages(async function (err, message, meta) {
-                // store the text from RocketChat incomming messages
-                // this message is already normalized.
-                // but we might be missing out on fields we want                 
-                message.type = await utils.getRoomType(meta, message, config.rocketchat_bot_mention_rooms, config.rocketchat_bot_user);
-                controller.ingest(bot, message)
-            }, options);
-        }
+  controller.startBot = async () => {
+    // implicit call for bot.defineBot()
+    var bot = controller.spawn(config)
+    try {
+      // make the connection with RocketChat
+      await driver.connect({ host: config.rocketchat_host, useSsl: SSL })
+      await driver.login({ username: config.rocketchat_bot_user, password: config.rocketchat_bot_pass })
+      await driver.joinRooms([config.rocketchat_bot_room])
+      await driver.subscribeToMessages()
+      bot.connected = true
+    } catch (error) {
+      bot.connected = false
+      console.log(error)
     }
 
-    controller.defineBot(function (botkit, config) {
-        var bot = {
-            type: 'rocketchat',
-            botkit: botkit,
-            config: config || {},
-            utterances: botkit.utterances,
+    if (bot.connected) {
+      var options = {
+        dm: config.rocketchat_bot_direct_messages,
+        livechat: config.rocketchat_bot_live_chat,
+        edited: config.rocketchat_bot_edited
+      }
+
+      // trigger when every message is sent from any source enabled from
+      // options
+      driver.respondToMessages(async function (err, message, meta) {
+        // store the text from RocketChat incomming messages
+        // this message is already normalized.
+        // but we might be missing out on fields we want
+        message.type = await utils.getRoomType(meta, message, config.rocketchat_bot_mention_rooms, config.rocketchat_bot_user)
+        controller.ingest(bot, message)
+      }, options)
+    }
+  }
+
+  controller.defineBot(function (botkit, config) {
+    var bot = {
+      type: 'rocketchat',
+      botkit: botkit,
+      config: config || {},
+      utterances: botkit.utterances
+    }
+
+    bot.send = async function (message, cb) {
+      var newMessage = {
+        msg: message.text,
+        attachments: message.attachments || []
+      }
+
+      if (bot.connected) {
+        // handles every type of message
+        if (message.type === 'direct_message') {
+          await driver.sendDirectToUser(newMessage, message.user)
+        } else if (message.type === 'live_chat') {
+          await driver.sendToRoomId(newMessage, message.channel)
+        } else if (message.type === 'mention') {
+          await driver.sendToRoomId(newMessage, message.channel)
+        } else if (message.type === 'channel') {
+          await driver.sendToRoomId(newMessage, message.channel)
+        } else if (message.type === 'message') {
+          await driver.sendToRoomId(newMessage, message.channel)
         }
+        cb()
+      }
+      // BOT is not connected
+      cb()
+    }
 
-        bot.send = async function (message, cb) {
-            var newMessage = {
-                msg: message.text,
-                attachments: message.attachments || []
-            }
-
-            if (bot.connected) {
-                // handles every type of message
-                if (message.type === 'direct_message') {
-                    await driver.sendDirectToUser(newMessage, message.user);
-                } else if (message.type === 'live_chat') {
-                    await driver.sendToRoomId(newMessage, message.channel);
-                } else if (message.type === 'mention') {
-                    await driver.sendToRoomId(newMessage, message.channel);
-                } else if (message.type === 'channel') {
-                    await driver.sendToRoomId(newMessage, message.channel);
-                } else if (message.type === 'message') {
-                    await driver.sendToRoomId(newMessage, message.channel);
-                }
-                cb();
-            }
-            // BOT is not connected
-            cb();
+    bot.reply = function (src, resp, cb) {
+      if (typeof (resp) === 'string') {
+        resp = {
+          text: resp
         }
+      }
+      resp.type = src.type
+      resp.user = src.user
+      resp.channel = src.channel
+      bot.say(resp, cb)
+    }
 
-        bot.reply = function (src, resp, cb) {
-            if (typeof (resp) == 'string') {
-                resp = {
-                    text: resp
-                };
-            }
-            resp.type = src.type;
-            resp.user = src.user;
-            resp.channel = src.channel;
-            bot.say(resp, cb);
-        };
-
-        // this function defines the mechanism by which botkit looks for ongoing conversations
-        // probably leave as is!
-        bot.findConversation = function (message, cb) {
-            for (var t = 0; t < botkit.tasks.length; t++) {
-                for (var c = 0; c < botkit.tasks[t].convos.length; c++) {
-                    if (
-                        botkit.tasks[t].convos[c].isActive() &&
-                        botkit.tasks[t].convos[c].source_message.user == message.user &&
-                        botkit.tasks[t].convos[c].source_message.channel == message.channel &&
-                        botkit.excludedEvents.indexOf(message.type) == -1 // this type of message should not be included
-                    ) {
-                        cb(botkit.tasks[t].convos[c]);
-                        return;
-                    }
-                }
-            }
-            cb();
-        };
-        return bot;
-    })
-
-    // Verify the pipeline of the message, using for debug
-    controller.middleware.receive.use(function (bot, message, next) { console.log('I RECEIVED', message); next(); });
-    controller.middleware.send.use(function (bot, message, next) { console.log('I AM SENDING', message); next(); });
-
-    // provide one or more normalize middleware functions that take a raw incoming message
-    // and ensure that the key botkit fields are present -- user, channel, text, and type
-    controller.middleware.normalize.use(function (bot, message, next) {
-        message.text = utils.handleMention(message, config.rocketchat_bot_user)
-        message.user = message.u.username
-        message.channel = message.rid
-        message.ts = message.ts.$date
-        next();
-    });
-
-    controller.middleware.categorize.use(function (bot, message, next) {
-        next();
-    });
-
-
-    // provide one or more ways to format outgoing messages from botkit messages into 
-    // the necessary format required by the platform API
-    // at a minimum, copy all fields from `message` to `platform_message`
-    controller.middleware.format.use(function (bot, message, platform_message, next) {
-        for (var k in message) {
-            platform_message[k] = message[k]
+    // this function defines the mechanism by which botkit looks for ongoing conversations
+    // probably leave as is!
+    bot.findConversation = function (message, cb) {
+      for (var t = 0; t < botkit.tasks.length; t++) {
+        for (var c = 0; c < botkit.tasks[t].convos.length; c++) {
+          if (
+            botkit.tasks[t].convos[c].isActive() &&
+                        botkit.tasks[t].convos[c].source_message.user === message.user &&
+                        botkit.tasks[t].convos[c].source_message.channel === message.channel &&
+                        botkit.excludedEvents.indexOf(message.type) === -1 // this type of message should not be included
+          ) {
+            cb(botkit.tasks[t].convos[c])
+            return
+          }
         }
-        if (!platform_message.type) {
-            platform_message.type = 'message';
-        }
-        next();
-    });
+      }
+      cb()
+    }
+    return bot
+  })
 
-    return controller;
+  // Verify the pipeline of the message, using for debug
+  controller.middleware.receive.use(function (bot, message, next) { console.log('I RECEIVED', message); next() })
+  controller.middleware.send.use(function (bot, message, next) { console.log('I AM SENDING', message); next() })
+
+  // provide one or more normalize middleware functions that take a raw incoming message
+  // and ensure that the key botkit fields are present -- user, channel, text, and type
+  controller.middleware.normalize.use(function (bot, message, next) {
+    message.text = utils.handleMention(message, config.rocketchat_bot_user)
+    message.user = message.u.username
+    message.channel = message.rid
+    message.ts = message.ts.$date
+    next()
+  })
+
+  controller.middleware.categorize.use(function (bot, message, next) {
+    next()
+  })
+
+  // provide one or more ways to format outgoing messages from botkit messages into
+  // the necessary format required by the platform API
+  // at a minimum, copy all fields from `message` to `platformMessage`
+  controller.middleware.format.use(function (bot, message, platformMessage, next) {
+    for (var k in message) {
+      platformMessage[k] = message[k]
+    }
+    if (!platformMessage.type) {
+      platformMessage.type = 'message'
+    }
+    next()
+  })
+
+  return controller
 }
 
-module.exports = RocketChatBot;
+module.exports = RocketChatBot
--- tests/RocketChatTest.js
+++ tests/RocketChatTest.js
@@ -1,10 +1,10 @@
-const should = require('should');
-const assert = require('assert');
-const Botkit = require('botkit');
-const rocketchatbot = require('../RocketChatBot.js');
-const utils = require('../utils.js');
+const should = require('should')
+const assert = require('assert')
+const Botkit = require('botkit')
+const rocketchatbot = require('../RocketChatBot.js')
+const utils = require('../utils.js')
 
-var bot_options = {
+var botOptions = {
   debug: false,
   studio_token: '',
   studio_command_uri: '',
@@ -17,87 +17,77 @@ var bot_options = {
   rocketchat_bot_direct_messages: 'true',
   rocketchat_bot_mention_rooms: 'channel1',
   rocketchat_bot_live_chat: 'true',
-  rocketchat_bot_edited: 'true',
-};
+  rocketchat_bot_edited: 'true'
+}
 
 describe('Initialize', function () {
   it('should have Botkit instance', function (done) {
-    should.exist(Botkit);
-    should.exist(Botkit.core);
-    done();
-  });
+    should.exist(Botkit)
+    should.exist(Botkit.core)
+    done()
+  })
 
   it('should have Botkit instance', function (done) {
-    should.exist(rocketchatbot);
-    done();
-  });
-
-});
+    should.exist(rocketchatbot)
+    done()
+  })
+})
 
 describe('Start', function () {
   it('should start and then stop', function (done) {
-    var controller = rocketchatbot({}, bot_options);
-    done();
-  });
-});
-
+    var controller = rocketchatbot({}, botOptions)
+    done()
+  })
+})
 
 describe('Utils functions', function () {
-  const correctBotName = 'botName';
+  const correctBotName = 'botName'
   const correctMessage = {
     mentions: [{ username: correctBotName }],
-    msg: '@botName this is a simple message.',
-  };
-  const correctMessageWithoutMention = {
-    mentions: [],
-    msg: 'this is a simple message.',
-  };
-  const correctRoomId = 'GENERAL';
-  const correctMetaChannel = {roomType: 'c'};
-  const correctMetaDirectMessage = {roomType: 'd'};
-  const correctMetaLiveChat = {roomType: 'l'};
-  const correctChannelList = 'channel';
-  const correctChannelListWithMoreChannels = 'channel1, channel2, GENERAL';
-
+    msg: '@botName this is a simple message.'
+  }
+  const correctMetaChannel = {roomType: 'c'}
+  const correctMetaDirectMessage = {roomType: 'd'}
+  const correctMetaLiveChat = {roomType: 'l'}
+  const correctChannelList = 'channel'
 
   it('should return true when the bot is metioned in the message', function () {
-    var result = utils.isMention(correctMessage, correctBotName);
+    var result = utils.isMention(correctMessage, correctBotName)
     assert.equal(true, result)
-  });
+  })
 
   it('should return false the bot is not mentioned', function () {
-    var result = utils.isMention(correctMessage, 'userMention');
+    var result = utils.isMention(correctMessage, 'userMention')
     assert.equal(false, result)
-  });
+  })
 
   it('should the message without mentions', function () {
-    var result = utils.handleMention(correctMessage, correctBotName);
+    var result = utils.handleMention(correctMessage, correctBotName)
     assert.equal('this is a simple message.', result)
-  });
+  })
 
   it('should the message without changes', function () {
-    var result = utils.handleMention(correctMessage, correctBotName);
+    var result = utils.handleMention(correctMessage, correctBotName)
     assert.equal('this is a simple message.', result)
-  });
+  })
 
   it('should return false with no parameters', async function () {
-    var result = await utils.isMentionRoom();
+    var result = await utils.isMentionRoom()
     assert.equal(false, result)
-  });
+  })
 
   it('should return channel', async function () {
-    var result = await utils.getRoomType(correctMetaChannel, correctMessage, correctChannelList, correctBotName);
+    var result = await utils.getRoomType(correctMetaChannel, correctMessage, correctChannelList, correctBotName)
     assert.equal('channel', result)
-  });
+  })
 
   it('should return direct_message', async function () {
-    var result = await utils.getRoomType(correctMetaDirectMessage, correctMessage, correctChannelList, correctBotName);
+    var result = await utils.getRoomType(correctMetaDirectMessage, correctMessage, correctChannelList, correctBotName)
     assert.equal('direct_message', result)
-  });
+  })
 
   it('should return live_chat', async function () {
-    var result = await utils.getRoomType(correctMetaLiveChat, correctMessage, correctChannelList, correctBotName);
+    var result = await utils.getRoomType(correctMetaLiveChat, correctMessage, correctChannelList, correctBotName)
     assert.equal('live_chat', result)
-  });
-});
-
+  })
+})
